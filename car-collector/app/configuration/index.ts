c4d7e7c8d438a773e86d9aae9150d9d11ff6e279
Olli Saarinen <>
2021-04-09 16:26:25 +0000

    Added sentry and some initial stuff

---
+++ car-collector/app/configuration/index.ts
@@ -0,0 +1,52 @@
+import * as dotenv from 'dotenv';
+
+/**
+ * Env configuration interface
+ *
+ * @interface IConfiguration
+ */
+interface IConfiguration {
+  aws: {
+    region: string;
+    url: string;
+    accessKey: string;
+    accessSecret: string;
+    carBucket: string;
+  };
+  carAPI: {
+    url: string;
+    user: string;
+    secret: string;
+  };
+  sentry: {
+    dsn: string;
+    env: string;
+  };
+}
+
+// Apply .env
+dotenv.config();
+
+/**
+ * Env configuration
+ *
+ * @interface IConfiguration
+ */
+export const Configuration: IConfiguration = {
+  aws: {
+    url: process.env.AWS_S3_URL as string,
+    region: process.env.AWS_S3_REGION as string,
+    accessKey: process.env.AWS_S3_ACCESS_KEY as string,
+    accessSecret: process.env.AWS_S3_ACCESS_SECRET as string,
+    carBucket: process.env.AWS_S3_CAR_BUCKET as string,
+  },
+  carAPI: {
+    url: process.env.NETTIAUTO_API_URL as string,
+    user: process.env.NETTIAUTO_API_USER as string,
+    secret: process.env.NETTIAUTO_API_SECRET as string,
+  },
+  sentry: {
+    dsn: process.env.SENTRY_DSN as string,
+    env: (process.env.SENTRY_ENV || process.env.NODE_ENV) as string,
+  },
+};
--- car-collector/app/handler.ts
+++ car-collector/app/handler.ts
@@ -2,9 +2,13 @@ import { Handler } from 'aws-lambda';
 import AWS from 'aws-sdk';
 import axios from 'axios';
 
+import { Configuration } from './configuration';
 import { Hello } from './models/hello';
+import { wrapHandler } from './utils/sentry';
 
-export const hello: Handler = (event: unknown) => {
+const { aws, carAPI } = Configuration;
+
+export const hello: Handler = wrapHandler(async (event) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify(
@@ -16,18 +20,19 @@ export const hello: Handler = (event: unknown) => {
       2,
     ),
   };
-
   return new Promise((resolve) => {
     resolve(response);
   });
-};
+});
 
 export const getCars: Handler = async () => {
+  const token = await getToken();
+
   const results = [];
-  for (let index2 = 0; index2 < 5; index2++) {
+  for (let index2 = 0; index2 < 1; index2++) {
     const arr: Promise<number>[] = [];
-    for (let index = 0; index < 25; index++) {
-      arr.push(getModels());
+    for (let index = 0; index < 1; index++) {
+      arr.push(searchCars(token));
     }
     const s = await Promise.all(arr);
     results.push(...s);
@@ -47,14 +52,13 @@ export const getCars: Handler = async () => {
   });
 };
 
-const getModels = async () => {
+const searchCars = async (token: string) => {
   const json = await axios
     .get(
-      'https://api.nettix.fi/rest/car/search?page=1&rows=100&sortBy=dateCreated&sortOrder=asc&latitude=60.5346&longitude=25.6074&isMyFavorite=false&make=230&includeMakeModel=true&accessoriesCondition=and&isPriced=true&vatDeduct=true&taxFree=false&kilometersTo=8000&tagsCondition=and&isAdDealerExchange=false',
+      `${carAPI.url}/rest/car/search?page=1&rows=100&sortBy=dateCreated&sortOrder=asc&latitude=60.5346&longitude=25.6074&isMyFavorite=false&make=230&includeMakeModel=true&accessoriesCondition=and&isPriced=true&vatDeduct=true&taxFree=false&kilometersTo=8000&tagsCondition=and&isAdDealerExchange=false`,
       {
         headers: {
-          'X-Access-Token':
-            'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpZCI6ImNlMTY5ZmI3YTFhMmU0MWIwZDgyYjU0ODQ0YWEzNzI2ODViOWRjYTUiLCJqdGkiOiJjZTE2OWZiN2ExYTJlNDFiMGQ4MmI1NDg0NGFhMzcyNjg1YjlkY2E1IiwiaXNzIjoiIiwiYXVkIjoiYW5vbnltb3VzIiwic3ViIjoiMjI3NDgzNyIsImV4cCI6MTYxNzY0MTc1NSwiaWF0IjoxNjE3NjM4MTU1LCJ0b2tlbl90eXBlIjoiYmVhcmVyIiwic2NvcGUiOiJyZWFkIHdyaXRlLXVzZXIiLCJ1c2VyX2dyb3VwIjoidXNlciIsImVtYWlsIjoib3NhYXJpbmU5M0BnbWFpbC5jb20iLCJuYW1lIjoiT2xsaSBTYWFyaW5lbiIsIm5ldHRpeF9pZCI6MjI3NDgzN30.Lo_HEpnyMu6sPc4p7CMkPQDLfp3yuvAHLEHXl3dOfipTm1tr4CvM-10Ms3hxfjKHQkBEZg1mJi8NGmaZxC1-A_R67tTgDMUZ5E4rUWvjvirhmwlr9whNjFZ0-sy0mUGw7hTr69IMY8KH9Ycm2e4j3BwRrDksmZTOU2Yn9M2bGwbv3ekAOweZzX5a89hPFJzqh9vK-XMqlqxemAKb2fCISbbZ4zSVq71Lq4OX0l3n2kff97FRAqrYcTkSiwmkn7PeTgeb52_i7BRKihuL_hVZadqHFnfefi6lQCW1f1949eSOOyNy6YVIBDDOhG-bXdntfNriigtjyKL7cpApnoII4A',
+          'X-Access-Token': token,
         },
       },
     )
@@ -66,16 +70,27 @@ const getModels = async () => {
   return json;
 };
 
-export const getBucket = (): void => {
-  AWS.config.update({ region: 'REGION' });
+const getToken = async (): Promise<string> => {
+  const token = await axios
+    .post(`${carAPI.url}/oauth2/token`, {
+      grant_type: 'password',
+      email: carAPI.user,
+      password: carAPI.secret,
+    })
+    .then((x) => x.data.token);
 
+  console.log(token);
+
+  return token;
+};
+
+export const getBucket = (): void => {
   // Create S3 service object
   const s3 = new AWS.S3({
-    apiVersion: '2006-03-01',
-    region: 'us-east-1',
-    endpoint: 'http://s3:9000',
-    accessKeyId: 'test',
-    secretAccessKey: 'supersecret',
+    region: aws.region,
+    endpoint: aws.url,
+    accessKeyId: aws.accessKey,
+    secretAccessKey: aws.accessSecret,
   });
 
   // Call S3 to list the buckets
