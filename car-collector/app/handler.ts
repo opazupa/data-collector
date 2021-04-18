bb40d30d09c484554c1742840a748ff96d22208e
Olli Saarinen <>
2021-04-18 15:17:52 +0300

    Bulk operation

--- car-collector/app/handler.ts
+++ car-collector/app/handler.ts
@@ -1,16 +1,49 @@
 import { Handler } from 'aws-lambda';
+import { DateTime } from 'luxon';
 
 import { getCars } from './services/cars';
 import { save } from './services/storage';
-import { today, wrapHandler } from './utils';
+import { wrapHandler } from './utils';
 
 /**
  * Save new cars from today to S3 storage
  */
 export const saveNewCars: Handler = wrapHandler(async () => {
-  const date = today();
+  const date = DateTime.utc().startOf('day');
   const cars = await getCars({ fromDate: date });
 
   // Save if we have results
   if (cars.length !== 0) await save(cars, date);
+});
+
+// Bulk operation paramaters
+export type BulkParams = {
+  from: string;
+  to: string;
+};
+
+/**
+ * Bulk import cars
+ */
+export const bulkImportCars: Handler<BulkParams> = wrapHandler(async (event) => {
+  let startDate = DateTime.fromISO(event.from, { zone: 'utc' }).startOf('day');
+  const endDate = DateTime.fromISO(event.to, { zone: 'utc' }).startOf('day');
+
+  // Validate input dates
+  if (!(startDate.isValid && endDate.isValid)) throw new Error('Invalid dates passed!');
+  const { days } = endDate.diff(startDate, ['days']).toObject();
+  if (days < 1) throw new Error('Too small gap between dates!');
+
+  console.log(`Bulk import for cars triggered with ${days} days.`);
+
+  // Handle every date in between
+  for (let progress = 1; progress <= days; progress++) {
+    console.log(`Progress: ${progress}/${days}. Getting cars for ${startDate.toISODate()}.`);
+    const cars = await getCars({ fromDate: startDate, toDate: startDate.endOf('day') });
+    // Save if we have results
+    if (cars.length !== 0) await save(cars, startDate);
+    // Move to next date
+    startDate = startDate.plus({ day: 1 });
+  }
+  console.log(`All set! Imported cars from ${days} days.`);
 });
--- car-collector/tests/handlers/handler.test.ts
+++ car-collector/tests/handlers/handler.test.ts
@@ -1,17 +1,20 @@
+import { expect } from 'chai';
 import lambdaTester from 'lambda-tester';
+import { DateTime } from 'luxon';
 import sinon from 'sinon';
 
-import { saveNewCars } from '../../app/handler';
+import { bulkImportCars, BulkParams, saveNewCars } from '../../app/handler';
 import { Car } from '../../app/models';
 import * as carAPI from '../../app/services/cars/api';
 import * as storage from '../../app/services/storage';
 
+const CARS = [
+  { model: 'Tesla', registerNumber: 'XXX-345' },
+  { model: 'Opel', registerNumber: 'OPO-341' },
+] as Car[];
+
 describe('Car collector handler', () => {
   it('Save new cars from yesterday', async () => {
-    const CARS = [
-      { model: 'Tesla', registerNumber: 'XXX-345' },
-      { model: 'Opel', registerNumber: 'OPO-341' },
-    ] as Car[];
     const carMock = sinon.mock(carAPI);
     const storageMock = sinon.mock(storage);
 
@@ -38,6 +41,104 @@ describe('Car collector handler', () => {
     await lambdaTester(saveNewCars)
       .event({})
       .expectResult(() => {
+        carMock.verify();
+        carMock.restore();
+        storageMock.verify();
+        storageMock.restore();
+      });
+  });
+
+  it('Bulk import cars', async () => {
+    const from = DateTime.utc().set({ day: -10 }).startOf('day').toISO();
+    const to = DateTime.utc().set({ day: -5 }).startOf('day').toISO();
+
+    const carMock = sinon.mock(carAPI);
+    const storageMock = sinon.mock(storage);
+
+    carMock.expects('getCars').exactly(5).returns(CARS);
+    storageMock.expects('save').exactly(5);
+
+    await lambdaTester(bulkImportCars)
+      .event({ from, to })
+      .expectResult(() => {
+        carMock.verify();
+        carMock.restore();
+        storageMock.verify();
+        storageMock.restore();
+      });
+  });
+
+  it('Bulk import cars with zero diff', async () => {
+    const from = DateTime.utc().set({ day: -5 }).startOf('day').toISO();
+    const to = from;
+
+    const carMock = sinon.mock(carAPI);
+    const storageMock = sinon.mock(storage);
+
+    carMock.expects('getCars').never();
+    storageMock.expects('save').never();
+
+    await lambdaTester(bulkImportCars)
+      .event({ from, to })
+      .expectError((err: Error) => {
+        expect(err.message).to.equal('Too small gap between dates!');
+        carMock.verify();
+        carMock.restore();
+        storageMock.verify();
+        storageMock.restore();
+      });
+  });
+
+  it('Bulk import cars with upside dates', async () => {
+    const from = DateTime.utc().set({ day: -5 }).startOf('day').toISO();
+    const to = DateTime.utc().set({ day: -10 }).startOf('day').toISO();
+
+    const carMock = sinon.mock(carAPI);
+    const storageMock = sinon.mock(storage);
+
+    carMock.expects('getCars').never();
+    storageMock.expects('save').never();
+
+    await lambdaTester(bulkImportCars)
+      .event({ from, to })
+      .expectError((err: Error) => {
+        expect(err.message).to.equal('Too small gap between dates!');
+        carMock.verify();
+        carMock.restore();
+        storageMock.verify();
+        storageMock.restore();
+      });
+  });
+
+  it('Bulk import cars with bad dates', async () => {
+    const carMock = sinon.mock(carAPI);
+    const storageMock = sinon.mock(storage);
+
+    carMock.expects('getCars').never();
+    storageMock.expects('save').never();
+
+    await lambdaTester(bulkImportCars)
+      .event({ from: '', to: '2012-07-14T01:00:00+01:00zs' })
+      .expectError((err: Error) => {
+        expect(err.message).to.equal('Invalid dates passed!');
+        carMock.verify();
+        carMock.restore();
+        storageMock.verify();
+        storageMock.restore();
+      });
+  });
+
+  it('Bulk import cars without dates', async () => {
+    const carMock = sinon.mock(carAPI);
+    const storageMock = sinon.mock(storage);
+
+    carMock.expects('getCars').never();
+    storageMock.expects('save').never();
+
+    await lambdaTester(bulkImportCars)
+      .event({} as BulkParams)
+      .expectError((err: Error) => {
+        expect(err.message).to.equal('Invalid dates passed!');
         carMock.verify();
         carMock.restore();
         storageMock.verify();
